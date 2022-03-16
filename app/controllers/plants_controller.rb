6709946cbe0594d2c98a5617917cd4c44d9cb324
nathalia <nathaliatroina@gmail.com>
2022-03-16 16:17:18 -0400

    commit tree 2d75efba433ec36745fd69ad87f0558459e8b9af

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+
+
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +20,23 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
-  private
+ # PATCH /plants/:id
+ def update
+  plant = Plant.find_by(id: params[:id])
+  plant.update(plant_params)
+  render json: plant
+end
 
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
-  end
+# DELETE /plants/:id
+def destroy
+  plant = Plant.find_by(id: params[:id])
+  plant.destroy
+  head :no_content
+end
+
+private
+
+def plant_params
+  params.permit(:name, :image, :price, :is_in_stock)
+end
 end
